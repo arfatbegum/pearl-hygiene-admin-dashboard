f605b4bc75fb6b202f3d73ee8a26c958386e2317
arfat132 <arfatbegum90@gmail.com>
2024-08-10 14:11:30 +0300

    updated

--- src/app/update-product/[id]/page.tsx
+++ src/app/update-product/[id]/page.tsx
@@ -56,7 +56,7 @@ const UpdateProduct = ({ params }: any) => {
                     setSpecifications(productData.specifications || []);
                     setImageUrls(productData.images || []);
                     setSelectedCategory(productData.productCategory || {});
-                    setPrimaryIndex(0); 
+                    setPrimaryIndex(0);
                 } else {
                     Swal.fire("Error!", `No product found with ID`, "error");
                 }
@@ -67,20 +67,21 @@ const UpdateProduct = ({ params }: any) => {
     const handleCategoryChange = (e: any) => {
         const selectedCategoryName = e.target.value;
         setProduct({ ...product, productCategory: selectedCategoryName });
-    
+
         const category = categories.find(
             // @ts-ignore
             (category) => category.name === selectedCategoryName
         );
-    
+
         if (category) {
             setSelectedCategory(category);
+            //@ts-ignore
             setSubCategories(category.subCategories || []);
         } else {
             setSubCategories([]);
         }
     };
-    
+
 
     const getBrandsFromFirebase = () => {
         const brandsRef = ref(database, 'brands');
@@ -126,7 +127,7 @@ const UpdateProduct = ({ params }: any) => {
             updatedSpecifications.splice(index, 1);
             setSpecifications(updatedSpecifications);
         } else {
-            Swal.fire("Error!",'Delete Cancelled', "error");
+            Swal.fire("Error!", 'Delete Cancelled', "error");
         }
     };
 
@@ -136,7 +137,7 @@ const UpdateProduct = ({ params }: any) => {
             updatedImages.splice(index, 1);
             setImageUrls(updatedImages);
         } else {
-            Swal.fire("Error!",'Delete Cancelled',"error");
+            Swal.fire("Error!", 'Delete Cancelled', "error");
         }
     };
 
@@ -155,11 +156,11 @@ const UpdateProduct = ({ params }: any) => {
         const productsRef = ref(database, `products/${id}`);
         update(productsRef, updatedProduct)
             .then(() => {
-               Swal.fire("Success!",'Product data updated successfully', "success");
+                Swal.fire("Success!", 'Product data updated successfully', "success");
                 router.push("/products")
             })
             .catch((error) => {
-                Swal.fire("Error!",`Error updating product data`, "error");
+                Swal.fire("Error!", `Error updating product data`, "error");
             });
     };
     return (
@@ -360,7 +361,7 @@ const UpdateProduct = ({ params }: any) => {
                                         // @ts-ignore
                                         value={product.productCategory || ''}
                                         onChange={handleCategoryChange}
-        
+
 
                                         className={`relative z-20 w-full appearance-none rounded border border-stroke bg-transparent px-5 py-3 outline-none transition focus:border-primary active:border-primary dark:border-form-strokedark dark:bg-form-input dark:focus:border-primary ${isOptionSelected ? "text-black dark:text-white" : ""
                                             }`}
@@ -483,7 +484,7 @@ const UpdateProduct = ({ params }: any) => {
                                         bullist numlist outdent indent | removeformat | help'
                                 }}
                             />
-        
+
                         </div>
 
                         <div className="mb-4.5">
@@ -495,28 +496,28 @@ const UpdateProduct = ({ params }: any) => {
                                 className="relative mb-5.5 block w-full cursor-pointer appearance-none rounded border border-dashed border-primary bg-gray px-4 py-4 dark:bg-meta-4 sm:py-7.5"
                             >
                                 <input
-                                  type="file"
-                                  id="image_upload"
-                                  onChange={(e) => {
-                                      // @ts-ignore
-                                      const file = e.target.files[0];
-                                      if (file) {
-                                          const formData = new FormData();
-                                          formData.append('image', file);
-                                          fetch(`https://api.imgbb.com/1/upload?key=${imgbbAPIKey}`, {
-                                              method: 'POST',
-                                              body: formData,
-                                          })
-                                              .then((response) => response.json())
-                                              .then((result) => {
-                                                  if (result.success) {
-                                                      // @ts-ignore
-                                                      setImageUrls([...imageUrls, result.data.url]);
-                                                  }
-                                              })
-                                              .catch((error) => console.error('Error uploading image:', error));
-                                      }
-                                  }}
+                                    type="file"
+                                    id="image_upload"
+                                    onChange={(e) => {
+                                        // @ts-ignore
+                                        const file = e.target.files[0];
+                                        if (file) {
+                                            const formData = new FormData();
+                                            formData.append('image', file);
+                                            fetch(`https://api.imgbb.com/1/upload?key=${imgbbAPIKey}`, {
+                                                method: 'POST',
+                                                body: formData,
+                                            })
+                                                .then((response) => response.json())
+                                                .then((result) => {
+                                                    if (result.success) {
+                                                        // @ts-ignore
+                                                        setImageUrls([...imageUrls, result.data.url]);
+                                                    }
+                                                })
+                                                .catch((error) => console.error('Error uploading image:', error));
+                                        }
+                                    }}
                                     className="absolute inset-0 z-50 m-0 h-full w-full cursor-pointer p-0 opacity-0 outline-none"
                                 />
                                 <div className="flex flex-col items-center justify-center space-y-3">
@@ -564,43 +565,43 @@ const UpdateProduct = ({ params }: any) => {
                                 <div key={index} className="">
                                     <img className="mb-5 h-24" src={url} alt={`Product Image ${index}`} width="100" height="100" />
                                     <div className="flex">
-                                    <button className="mr-3" type="button" onClick={() => handleImageDelete(index)}>
-                                        <svg
-                                            className="fill-current "
-                                            width="18"
-                                            height="18"
-                                            viewBox="0 0 18 18"
-                                            fill="red"
-                                            xmlns="http://www.w3.org/2000/svg"
-                                        >
-                                            <path
-                                                d="M13.7535 2.47502H11.5879V1.9969C11.5879 1.15315 10.9129 0.478149 10.0691 0.478149H7.90352C7.05977 0.478149 6.38477 1.15315 6.38477 1.9969V2.47502H4.21914C3.40352 2.47502 2.72852 3.15002 2.72852 3.96565V4.8094C2.72852 5.42815 3.09414 5.9344 3.62852 6.1594L4.07852 15.4688C4.13477 16.6219 5.09102 17.5219 6.24414 17.5219H11.7004C12.8535 17.5219 13.8098 16.6219 13.866 15.4688L14.3441 6.13127C14.8785 5.90627 15.2441 5.3719 15.2441 4.78127V3.93752C15.2441 3.15002 14.5691 2.47502 13.7535 2.47502ZM7.67852 1.9969C7.67852 1.85627 7.79102 1.74377 7.93164 1.74377H10.0973C10.2379 1.74377 10.3504 1.85627 10.3504 1.9969V2.47502H7.70664V1.9969H7.67852ZM4.02227 3.96565C4.02227 3.85315 4.10664 3.74065 4.24727 3.74065H13.7535C13.866 3.74065 13.9785 3.82502 13.9785 3.96565V4.8094C13.9785 4.9219 13.8941 5.0344 13.7535 5.0344H4.24727C4.13477 5.0344 4.02227 4.95002 4.02227 4.8094V3.96565ZM11.7285 16.2563H6.27227C5.79414 16.2563 5.40039 15.8906 5.37227 15.3844L4.95039 6.2719H13.0785L12.6566 15.3844C12.6004 15.8625 12.2066 16.2563 11.7285 16.2563Z"
+                                        <button className="mr-3" type="button" onClick={() => handleImageDelete(index)}>
+                                            <svg
+                                                className="fill-current "
+                                                width="18"
+                                                height="18"
+                                                viewBox="0 0 18 18"
                                                 fill="red"
+                                                xmlns="http://www.w3.org/2000/svg"
+                                            >
+                                                <path
+                                                    d="M13.7535 2.47502H11.5879V1.9969C11.5879 1.15315 10.9129 0.478149 10.0691 0.478149H7.90352C7.05977 0.478149 6.38477 1.15315 6.38477 1.9969V2.47502H4.21914C3.40352 2.47502 2.72852 3.15002 2.72852 3.96565V4.8094C2.72852 5.42815 3.09414 5.9344 3.62852 6.1594L4.07852 15.4688C4.13477 16.6219 5.09102 17.5219 6.24414 17.5219H11.7004C12.8535 17.5219 13.8098 16.6219 13.866 15.4688L14.3441 6.13127C14.8785 5.90627 15.2441 5.3719 15.2441 4.78127V3.93752C15.2441 3.15002 14.5691 2.47502 13.7535 2.47502ZM7.67852 1.9969C7.67852 1.85627 7.79102 1.74377 7.93164 1.74377H10.0973C10.2379 1.74377 10.3504 1.85627 10.3504 1.9969V2.47502H7.70664V1.9969H7.67852ZM4.02227 3.96565C4.02227 3.85315 4.10664 3.74065 4.24727 3.74065H13.7535C13.866 3.74065 13.9785 3.82502 13.9785 3.96565V4.8094C13.9785 4.9219 13.8941 5.0344 13.7535 5.0344H4.24727C4.13477 5.0344 4.02227 4.95002 4.02227 4.8094V3.96565ZM11.7285 16.2563H6.27227C5.79414 16.2563 5.40039 15.8906 5.37227 15.3844L4.95039 6.2719H13.0785L12.6566 15.3844C12.6004 15.8625 12.2066 16.2563 11.7285 16.2563Z"
+                                                    fill="red"
+                                                />
+                                                <path
+                                                    d="M9.00039 9.11255C8.66289 9.11255 8.35352 9.3938 8.35352 9.75942V13.3313C8.35352 13.6688 8.63477 13.9782 9.00039 13.9782C9.33789 13.9782 9.64727 13.6969 9.64727 13.3313V9.75942C9.64727 9.3938 9.33789 9.11255 9.00039 9.11255Z"
+                                                    fill="red"
+                                                />
+                                                <path
+                                                    d="M11.2502 9.67504C10.8846 9.64692 10.6033 9.90004 10.5752 10.2657L10.4064 12.7407C10.3783 13.0782 10.6314 13.3875 10.9971 13.4157C11.0252 13.4157 11.0252 13.4157 11.0533 13.4157C11.3908 13.4157 11.6721 13.1625 11.6721 12.825L11.8408 10.35C11.8408 9.98442 11.5877 9.70317 11.2502 9.67504Z"
+                                                    fill="red"
+                                                />
+                                                <path
+                                                    d="M6.72245 9.67504C6.38495 9.70317 6.1037 10.0125 6.13182 10.35L6.3287 12.825C6.35683 13.1625 6.63808 13.4157 6.94745 13.4157C6.97558 13.4157 6.97558 13.4157 7.0037 13.4157C7.3412 13.3875 7.62245 13.0782 7.59433 12.7407L7.39745 10.2657C7.39745 9.90004 7.08808 9.64692 6.72245 9.67504Z"
+                                                    fill=""
+                                                />
+                                            </svg>
+                                        </button>
+                                        <div className="flex justify-between gap-1 items-center">
+                                            <input
+                                                type="radio"
+                                                name="primaryImage"
+                                                checked={index === primaryIndex}
+                                                onChange={() => handlePrimaryImageChange(index)}
                                             />
-                                            <path
-                                                d="M9.00039 9.11255C8.66289 9.11255 8.35352 9.3938 8.35352 9.75942V13.3313C8.35352 13.6688 8.63477 13.9782 9.00039 13.9782C9.33789 13.9782 9.64727 13.6969 9.64727 13.3313V9.75942C9.64727 9.3938 9.33789 9.11255 9.00039 9.11255Z"
-                                                fill="red"
-                                            />
-                                            <path
-                                                d="M11.2502 9.67504C10.8846 9.64692 10.6033 9.90004 10.5752 10.2657L10.4064 12.7407C10.3783 13.0782 10.6314 13.3875 10.9971 13.4157C11.0252 13.4157 11.0252 13.4157 11.0533 13.4157C11.3908 13.4157 11.6721 13.1625 11.6721 12.825L11.8408 10.35C11.8408 9.98442 11.5877 9.70317 11.2502 9.67504Z"
-                                                fill="red"
-                                            />
-                                            <path
-                                                d="M6.72245 9.67504C6.38495 9.70317 6.1037 10.0125 6.13182 10.35L6.3287 12.825C6.35683 13.1625 6.63808 13.4157 6.94745 13.4157C6.97558 13.4157 6.97558 13.4157 7.0037 13.4157C7.3412 13.3875 7.62245 13.0782 7.59433 12.7407L7.39745 10.2657C7.39745 9.90004 7.08808 9.64692 6.72245 9.67504Z"
-                                                fill=""
-                                            />
-                                        </svg>
-                                    </button>
-                                    <div className="flex justify-between gap-1 items-center">
-                                    <input
-                                        type="radio"
-                                        name="primaryImage"
-                                        checked={index === primaryIndex}
-                                        onChange={() => handlePrimaryImageChange(index)}
-                                    />
-                                   <p>Primary</p>
-                                   </div>
-                                  </div>
+                                            <p>Primary</p>
+                                        </div>
+                                    </div>
                                 </div>
                             ))}
                         </div>
